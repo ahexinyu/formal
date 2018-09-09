d784dbcf4ef5b41b538066a215d301ce3dbf59cd
ahexinyu <466656393@qq.com>
2018-09-09 21:02:13 +0800

    eee

--- MECAT-master_1/src/mecat2ref/mecat2ref_aux.h
+++ MECAT-master_1/src/mecat2ref/mecat2ref_aux.h
@@ -42,7 +42,7 @@ AlignInfoContained(const AlignInfo& a, const AlignInfo& b)
 }
 
 int 
-find_location(int *t_loc,int *t_seedn,int *t_score,long *loc,int k,int *rep_loc,float len,int read_len1, double ddfs_cutoff,sim sc,long start_loc);
+find_location(int *t_loc,int *t_seedn,int *t_score,long *loc,int k,int *rep_loc,float len,int read_len1, double ddfs_cutoff,sim *sc,long start_loc);
 int
 find_location2(int *t_loc,int *t_seedn,int *t_score,long *loc,int k,int *rep_loc,float len,int read_len1, double ddfs_cutoff);
 bool extend_candidate(candidate_save& can,
