914dc5113dc0b82782c231529b93c12a60a625de
ahexinyu <466656393@qq.com>
2018-11-16 18:01:44 +0800

    wwww

--- MECAT-master_1/src/mecat2ref/mecat2ref_impl_large.cpp
+++ MECAT-master_1/src/mecat2ref/mecat2ref_impl_large.cpp
@@ -866,7 +866,8 @@ static void reference_mapping(int threadint)
                                         temp_spr->loczhi[loc-1]=u_k;//位置，block位置。
                                         temp_spr->seedno[loc-1]=k+1;
                                     }
-                                    else insert_loc(temp_spr,u_k,k+1,BC,sc,templong);//删除分数最小的。保持在20个左右//
+                                   // else insert_loc(temp_spr,u_k,k+1,BC,sc,templong);//删除分数最小的。保持在20个左右//
+                                    else insert_loc(temp_spr,u_k,k+1,BC);
                                     if(templong>0)s_k=temp_spr->score+(temp_spr-1)->score;
                                     else s_k=temp_spr->score;
                                     if(endnum<s_k)endnum=s_k;
@@ -924,7 +925,8 @@ static void reference_mapping(int threadint)
                                 u_k++;
                             }
                         }
-                        flag_end=find_location(temp_list,temp_seedn,temp_score,location_loc,u_k,&repeat_loc,BC,read_len, ddfs_cutoff,sc,start_loc);
+                       //flag_end=find_location(temp_list,temp_seedn,temp_score,location_loc,u_k,&repeat_loc,BC,read_len, ddfs_cutoff,sc,start_loc);
+                        flag_end=find_location2(temp_list,temp_seedn,temp_score,location_loc,u_k,&repeat_loc,BC,read_len, ddfs_cutoff);
                         if(flag_end==0)continue;
                         if(temp_score[repeat_loc]<6)continue;
                         canidate_temp.score=temp_score[repeat_loc];
@@ -1131,7 +1133,8 @@ static void reference_mapping(int threadint)
                                             temp_spr->loczhi[loc-1]=u_k;
                                             temp_spr->seedno[loc-1]=k+1;
                                         }
-                                        else insert_loc3(temp_spr,u_k,k+1,BC,sc,templong);
+                                        //else insert_loc3(temp_spr,u_k,k+1,BC,sc,templong);
+                                        else insert_loc2(temp_spr,u_k,k+1,BC);
                                         if(templong>0)s_k=temp_spr->score+(temp_spr-1)->score;
                                         else s_k=temp_spr->score;
                                         if(endnum<s_k)endnum=s_k;
@@ -1188,7 +1191,8 @@ static void reference_mapping(int threadint)
                                     u_k++;
                                 }
                             }
-                            flag_end=find_location(temp_list,temp_seedn,temp_score,location_loc,u_k,&repeat_loc,BC,read_len, ddfs_cutoff,sc,start_loc);
+                            //flag_end=find_location(temp_list,temp_seedn,temp_score,location_loc,u_k,&repeat_loc,BC,read_len, ddfs_cutoff,sc,start_loc);
+                            flag_end=find_location(temp_list,temp_seedn,temp_score,location_loc,u_k,&repeat_loc,BC,read_len, ddfs_cutoff);
                             if(flag_end==0)continue;
                             if(temp_score[repeat_loc]<6)continue;
                             canidate_temp.score=temp_score[repeat_loc];
