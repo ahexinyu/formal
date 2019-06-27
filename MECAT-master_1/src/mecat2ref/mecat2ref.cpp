c7ecd0913d7ca68312b1cd0ba18cc2a0d72926f0
ahexinyu <466656393@qq.com>
2019-06-27 14:17:48 +0800

    ssssssss

--- MECAT-master_1/src/mecat2ref/mecat2ref.cpp
+++ MECAT-master_1/src/mecat2ref/mecat2ref.cpp
@@ -681,10 +681,11 @@ void polish_result(const char *workpath,int filecount,int refcount,char  *refout
                         if(1){
                             for (int k=0; k<num_results; k++) {
                                 sid2=get_chr_id(chr_idx, num_chr, pptr[k]->sb);
-                                if (sid==sid2&&pptr[j]->qb!=pptr[k]->qb) {
+                                if (sid==sid2&&pptr[j]->qb!=pptr[k]->qb&&labs(pptr[j]->sb-pptr[k]->sb)>2000) {
                                     pptr[j]->sb=org_sta;
                                     pptr[j]->se=org_end;
                                     output_temp_result2(pptr[j],out,ref_name,ref_size);
+                                    break;
                                 }
                             }
                             /*for(int k=0;k<num_results;k++){
