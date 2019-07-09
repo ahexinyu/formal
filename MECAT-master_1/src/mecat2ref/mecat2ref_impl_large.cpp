c95c152a682b211017b7f3e246c4bfea3b8eefc8
ahexinyu <466656393@qq.com>
2019-07-09 20:32:49 +0800

    rrrrrr

--- MECAT-master_1/src/mecat2ref/mecat2ref_impl_large.cpp
+++ MECAT-master_1/src/mecat2ref/mecat2ref_impl_large.cpp
@@ -970,6 +970,7 @@ static void reference_mapping(int threadint)
 
 			naln = 0;
 			nresults = 0;
+            printf("%d\n",canidate_loc[0]);
             for(i=0; i<canidatenum; i++)
             {
 				extend_candidate(canidate_loc[i], 
@@ -1235,7 +1236,7 @@ static void reference_mapping(int threadint)
                 
 				naln = 0;
 				nresults = 0;
-                printf("%d\n",canidate_loc[0]);
+                
                 for(i=0; i<canidatenum; i++)
                 {
 					extend_candidate(canidate_loc[i], 
