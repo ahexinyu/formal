26bb8b377be5dcf3570621d2f02edafe8af3350e
ahexinyu <466656393@qq.com>
2019-08-06 20:51:52 +0800

    eeeeeee

--- MECAT-master_1/src/mecat2ref/mecat2ref_impl_large.cpp
+++ MECAT-master_1/src/mecat2ref/mecat2ref_impl_large.cpp
@@ -582,7 +582,7 @@ static void get_vote(){
             sc[j].vote=1;
         }
         else{
-            deviation=fabs(1-sc[j].k_count/ave_count);
+            deviation=1-log(ave_count/sc[j].k_count);
             sc[j].vote=deviation;
         }
        //deviation=sqrt(pow((sc[j].k_count-ave_count),2)/similarity_count);//方差
