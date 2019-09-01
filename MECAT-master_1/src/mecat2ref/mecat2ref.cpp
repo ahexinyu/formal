cbd91c35dd2330563823aeaf0f56f0a591aac89a
ahexinyu <466656393@qq.com>
2019-09-01 23:20:23 +0800

    ppppp

--- MECAT-master_1/src/mecat2ref/mecat2ref.cpp
+++ MECAT-master_1/src/mecat2ref/mecat2ref.cpp
@@ -788,7 +788,7 @@ void polish_result(const char *workpath,int filecount,int refcount,char  *refout
                     }
                     
                 }
-                output_query_results(chr_idx, num_chr, out_pptr,num_results, out);//shuchu
+                output_query_results(chr_idx, num_chr, out_pptr,p_num, out);//shuchu
                 num_results=0;
                 p_num=0;
                for(int i=0;i<10;i++){mark[i]=0;vote[i]=0;}
