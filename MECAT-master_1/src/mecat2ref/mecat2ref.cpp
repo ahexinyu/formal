562c3c90969a8aaacb4974830ef8838225c5af0e
ahexinyu <466656393@qq.com>
2020-05-06 17:24:21 +0800

    wwwww

--- MECAT-master_1/src/mecat2ref/mecat2ref.cpp
+++ MECAT-master_1/src/mecat2ref/mecat2ref.cpp
@@ -316,7 +316,7 @@ int chang_fastqfile(const char *fastaq, const char *fenfolder)
     int num_read_items;
     ch=getc(fp);int kk=0;
      
-    if(ch=='>')
+  /*  if(ch=='>')
     {
         kk=0;
         for (; ch!=EOF; ch=getc(fp))
@@ -359,7 +359,7 @@ int chang_fastqfile(const char *fastaq, const char *fenfolder)
     fclose(ot);
     free(fq);
     free(oq);
-    return (kk);
+    return (kk);*/
      //return 1;
 }
 
