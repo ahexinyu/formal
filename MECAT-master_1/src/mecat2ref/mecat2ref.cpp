635f4346d5a2e4637e34a4d84ec38013adc41877
ahexinyu <466656393@qq.com>
2020-05-06 17:16:40 +0800

    qqqqq

--- MECAT-master_1/src/mecat2ref/mecat2ref.cpp
+++ MECAT-master_1/src/mecat2ref/mecat2ref.cpp
@@ -306,13 +306,13 @@ int chang_fastqfile(const char *fastaq, const char *fenfolder)
     sprintf(tempstr,"%s/ref.fq",outpath);
     ot=fopen(tempstr,"w");
      
-     onedata=(char *)malloc(9000000000);
-     buff2=(char *)malloc(900000000);
-     buff1=(char *)malloc(900000000);
-    fq = (char *)malloc(9000000000);
-    setvbuf(fp, fq, _IOFBF, 9000000000);
-    oq = (char *)malloc(9000000000);
-    setvbuf(ot, oq, _IOFBF, 9000000000);
+     onedata=(char *)malloc(90000000000);
+     buff2=(char *)malloc(9000000000);
+     buff1=(char *)malloc(9000000000);
+    fq = (char *)malloc(90000000000);
+    setvbuf(fp, fq, _IOFBF, 90000000000);
+    oq = (char *)malloc(90000000000);
+    setvbuf(ot, oq, _IOFBF, 90000000000);
     int num_read_items;
     ch=getc(fp);int kk=0;
      
